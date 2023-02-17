2e467a7f9d407a6aba37f841168452307730e518
Warren R Bank <warren.r.bank@gmail.com>
2023-02-16 18:33:14 -0800

    fix error in "config.php"

--- docker/config.php
+++ docker/config.php
@@ -1,41 +1,53 @@
 <?php
 
-if (!isset($_ENV["REDIS_HOST"]))
+if (empty(getenv("REDIS_HOST", true)))
   die("Required environment variable is not defined: REDIS_HOST\n");
-if (!isset($_ENV["REDIS_PORT"]))
+if (empty(getenv("REDIS_PORT", true)))
   die("Required environment variable is not defined: REDIS_PORT\n");
-if ((get_auth_method() == PASSWORD) && !isset($_ENV["PASSWORD_HASH"]))
+if ((get_auth_method() == PASSWORD) && empty(getenv("PASSWORD_HASH", true)))
   die("Required environment variable is not defined: PASSWORD_HASH\n");
-if (!isset($_ENV["PUBLIC_URL"]))
+if (empty(getenv("PUBLIC_URL", true)))
   die("Required environment variable is not defined: PUBLIC_URL\n");
 
 function get_auth_method() {
-    return (
-        isset($_ENV["AUTH_METHOD"]) &&
-        (strcmp($_ENV["AUTH_METHOD"], "HTPASSWD") == 0)
-    ) ? HTPASSWD : PASSWORD;
+    $auth_method   = getenv("AUTH_METHOD", true);
+    $default_value = PASSWORD;
+
+    if (empty($auth_method))
+        return $default_value;
+    else if (strcmp($auth_method, "PASSWORD") == 0)
+        return PASSWORD;
+    else if (strcmp($auth_method, "HTPASSWD") == 0)
+        return HTPASSWD;
+    else
+        return $default_value;
 }
 
 function get_velocity_unit() {
-    if (!isset($_ENV["VELOCITY_UNIT"]))
-        return MILES_PER_HOUR;
-    else if (strcmp($_ENV["VELOCITY_UNIT"], "METERS_PER_SECOND") == 0)
-        return METERS_PER_SECOND;
-    else if (strcmp($_ENV["VELOCITY_UNIT"], "KILOMETERS_PER_HOUR") == 0)
+    $velocity_unit = getenv("VELOCITY_UNIT", true);
+    $default_value = MILES_PER_HOUR;
+
+    if (empty($velocity_unit))
+        return $default_value;
+    else if (strcmp($velocity_unit, "KILOMETERS_PER_HOUR") == 0)
         return KILOMETERS_PER_HOUR;
-    else
+    else if (strcmp($velocity_unit, "MILES_PER_HOUR") == 0)
         return MILES_PER_HOUR;
+    else if (strcmp($velocity_unit, "METERS_PER_SECOND") == 0)
+        return METERS_PER_SECOND;
+    else
+        return $default_value;
 }
 
 const CONFIG = array(
     "storage_backend"       => REDIS,
-    "redis_host"            => constant($_ENV["REDIS_HOST"]),
-    "redis_port"            => constant($_ENV["REDIS_PORT"]),
+    "redis_host"            => constant(getenv("REDIS_HOST", true)),
+    "redis_port"            => constant(getenv("REDIS_PORT", true)),
     "redis_use_auth"        => false,
     "redis_auth"            => '',
     "redis_prefix"          => 'hauk',
     "auth_method"           => constant(get_auth_method()),
-    "password_hash"         => constant($_ENV["PASSWORD_HASH"]),
+    "password_hash"         => constant(getenv("PASSWORD_HASH", true)),
     "velocity_unit"         => constant(get_velocity_unit()),
-    "public_url"            => constant($_ENV["PUBLIC_URL"])
+    "public_url"            => constant(getenv("PUBLIC_URL", true))
 );
